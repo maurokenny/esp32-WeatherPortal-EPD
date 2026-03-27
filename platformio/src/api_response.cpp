f7912d69b148f9a48f3e0cdd01c29fb3fa0491c7
Mauro de Freitas <maurofreitas@>
2026-03-27 22:40:48 +0100

    Fix precipitation_probability

--- platformio/src/api_response.cpp
+++ platformio/src/api_response.cpp
@@ -739,11 +739,8 @@ DeserializationError deserializeOpenMeteo(WiFiClient &json,
   JsonArray hourly_wind = hourly["wind_speed_10m"];
   JsonArray hourly_gust = hourly["wind_gusts_10m"];
   JsonArray hourly_deg = hourly["wind_direction_10m"];
-#ifdef UNITS_HOURLY_PRECIP_POP
   JsonArray hourly_pop = hourly["precipitation_probability"];
-#else
   JsonArray hourly_precip = hourly["precipitation"];
-#endif
   JsonArray hourly_code = hourly["weather_code"];
   JsonArray hourly_is_day = hourly["is_day"];
   
@@ -760,18 +757,10 @@ DeserializationError deserializeOpenMeteo(WiFiClient &json,
     r.hourly[i].wind_gust = hourly_gust[i].as<float>();
     r.hourly[i].wind_deg = hourly_deg[i].as<int>();
     
-#ifdef UNITS_HOURLY_PRECIP_POP
-    // Handle precipitation_probability - check if null
     float pop_val = hourly_pop[i].as<float>();
     r.hourly[i].pop = pop_val / 100.0f; // Convert % to 0-1
-    r.hourly[i].rain_1h = 0.0f; // Not used in POP mode
-    r.hourly[i].snow_1h = 0.0f;
-#else
-    // Read actual precipitation (mm)
-    r.hourly[i].pop = 0.0f; // Not used in volume mode
     r.hourly[i].rain_1h = hourly_precip[i].as<float>();
     r.hourly[i].snow_1h = 0.0f; // Open-Meteo doesn't separate snow in basic endpoint
-#endif
     
     bool hourIsDay = hourly_is_day[i].as<int>() == 1;
     wmoToOwmWeather(hourly_code[i].as<int>(), hourIsDay, r.hourly[i].weather);
--- platformio/src/client_utils.cpp
+++ platformio/src/client_utils.cpp
@@ -165,11 +165,7 @@ bool waitForSNTPSync(tm *timeInfo)
                "cloud_cover,pressure_msl,surface_pressure,wind_speed_10m,"
                "wind_direction_10m,wind_gusts_10m,visibility,uv_index"
                "&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,"
-#ifdef UNITS_HOURLY_PRECIP_POP
-               "precipitation_probability,"
-#else
-               "precipitation,"
-#endif
+               "precipitation_probability,precipitation,"
                "weather_code,cloud_cover,surface_pressure,"
                "wind_speed_10m,wind_direction_10m,wind_gusts_10m,is_day"
                "&daily=weather_code,temperature_2m_max,temperature_2m_min,"
